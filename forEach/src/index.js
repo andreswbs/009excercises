f4a49cde0326ea8024cc76aacc4a8099893274de
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-05-18 20:53:25 +0300

    myForEach

--- forEach/src/index.js
+++ forEach/src/index.js
@@ -11,3 +11,15 @@ const outputValue = function (x) {
 //forEach; give me a function that I will call for each element of myArray
 myArray.forEach(outputValue)
 console.log(allElements)
+
+console.log("-->Now for our own forEach:")
+
+//We demand, that iteratorFunction must have two parameters: value, index
+function myForEach(parameterArray, iteratorFunction) {
+    for (let i=0; i<parameterArray.length; i++ ) {
+        iteratorFunction(parameterArray[i], i)
+    }
+}
+
+
+myForEach(myArray, outputValue)
