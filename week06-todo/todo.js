00a100b8056a4d7d0b45c1cc3b4b526c68255f5d
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-05-28 16:15:49 +0300

    introducing objects, on click responses

--- week06-todo/todo.js
+++ week06-todo/todo.js
@@ -1,14 +1,35 @@
-const tasksArray = [
-    "make coffe",
-    "walk the dog",
-    "make breakfast"
-]
+const task1 = {
+    description: "make coffe",
+    isCompleted: false,
+    priority: 6
+}
 
-function makeTaskHTML(task) {
+const task2 = {
+    description: "walk the dog",
+    isCompleted: true,
+    priority: 8
+}
+
+const task3 = {
+    description: "make breakfast",
+    isCompleted: true,
+    priority: 8,
+    day: Monday,
+    date: "20220528"
+}
+
+const tasksArray = [task1, task2, task3]
+
+function makeTaskHTML(taskObj, index) {
+    let isChecked = ""
+    if (taskObj.isCompleted) {
+        isChecked = 'checked'
+    }
+
     return `
         <div>
-            <input type="checkbox" class="form-check-input">
-            ${task}
+            <input type="checkbox" onclick="rowClicked(${index})" class="form-check-input" ${isChecked}>
+            ${taskObj.description} ( ${taskObj.priority} )
         </div>
     `
 }
@@ -16,10 +37,29 @@ function makeTaskHTML(task) {
 function renderTasks() {
     const outputElement = document.getElementById("output")
     outputElement.innerHTML = ''
-    tasksArray.forEach((task) => {
+    tasksArray.forEach((task, rowIndex) => {
         console.log(task)
-        outputElement.innerHTML += makeTaskHTML(task)
+        outputElement.innerHTML += makeTaskHTML(task, rowIndex)
     })
+
+    const doneTasks = tasksArray.filter((taskObj)=>{
+        return taskObj.isCompleted
+    })
+
+    outputElement.innerHTML += `
+        <div class="mt-2">
+            Done tasks: ${doneTasks.length}
+        </div>
+    `
+    console.log(doneTasks)
+}
+
+function rowClicked(rowIndex) {
+    console.log("Row clicked: " + rowIndex)
+    const taskObj = tasksArray[rowIndex]
+    console.log(taskObj)
+    taskObj.isCompleted = !taskObj.isCompleted
+    renderTasks()
 }
 
 function addTask() {
@@ -30,7 +70,14 @@ function addTask() {
         document.getElementById("message").style.display = "block"
         return
     }
-    tasksArray.push(inputElement.value)
+
+    const newTask = {
+        description: inputElement.value,
+        isCompleted: false,
+        priority: 6
+    }
+
+    tasksArray.push(newTask)
     inputElement.value = ""
     document.getElementById("message").innerText = ""
     document.getElementById("message").style.display = "none"
