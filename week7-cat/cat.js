262a1161dbc18f68382aa904c6960e5f10cb9eb3
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-06-02 21:57:28 +0300

    cat, start of reading list

--- week7-cat/cat.js
+++ week7-cat/cat.js
@@ -13,7 +13,7 @@ class Cat {
         if (this.hunger < 0) {
             this.hunger = 0
         }
-        
+        this.outputCat()
     }
     sleep() {
         this.tiredness -= 1
@@ -21,6 +21,7 @@ class Cat {
         if (this.tiredness < 0) {
             this.tiredness = 0
         }
+        this.outputCat()
     }
     pet() {
         this.loneliness -= 1
@@ -28,13 +29,16 @@ class Cat {
         if (this.loneliness < 0) {
             this.loneliness = 0
         }
+        this.outputCat()
     }
 
     timePasses() {
+        console.log("Tine passes ...")
         this.happiness -= 3
         this.loneliness += 1
         this.hunger += 1
         this.tiredness += 1
+        this.outputCat()
     }
 
     getCatDescription() {
@@ -48,8 +52,27 @@ class Cat {
 
     outputCat() {
         console.log(this.getCatDescription())
+        const catElement = document.getElementById('catdisplay')
+        catElement.innerHTML = `
+        <div>tiredness: ${this.tiredness}</div>
+        <div>hunger: ${this.hunger}</div>
+        <div>loneliness: ${this.loneliness}</div>
+        <div>happiness: ${this.happiness}</div>
+        `
     }
 }
 
 const myCat = new Cat('Neti')
 myCat.outputCat()
+setInterval(() => {myCat.timePasses()}, 5000)
+
+//1. Render cat status as html page at the start
+// 1.1 create html for each state variable <- modify function output cat
+// hint: 
+//2. Create html buttons and add event listeners 
+// to them that will call cat methods (feed, sleep, pet)
+// myCat.feed() Example: <a href="#" onclick="myCat.feed()" >Feed</a>
+// 2.1 After call to cat methods render cat status data again
+// hint: inside of the cat methods feed, sleep, pet call rendering function 
+//       from object itself: this.outputCat()
+//3. create interval that calls timePasses in every 3 seconds and renders all cat data again
---
+++ week7-readlingList/books.js
@@ -0,0 +1,17 @@
+class BookList {
+
+}
+
+class Book {
+
+}
+
+//1. Create class Book. Add data to the class
+//2. Create book list. Initially book list can empty
+//2.1. Add booklist data: booksArray, readCount, currentBookIndex
+// hint: currentBookIndex can be index of an element in booksArray
+//2.2 Add function getUnreadCount that should return number unread books
+//2.3 Add a function add(book) this function should add a book object to booksArray
+//2.4 Test in console that adding a book works
+
+
